5ef7cb1f3ec2f4750d0dec4411643f176a682091
HoJun-Seo <ghwns6659@gmail.com>
2022-02-03 23:45:14 +0900

    ES6

---
+++ ES6_Study/test.js
@@ -0,0 +1,7 @@
+var var1 = document.getElementById("goorm");
+
+console.log(var1.getAttribute("href"));
+
+// 속성값에 특정한 값을 세팅
+var1.setAttribute("href", "https://github.com/HoJun-Seo");
+console.log(var1.getAttribute("href"));
